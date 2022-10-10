02e2275d1c5b28b14940d9ababdd73b0e0af76ad
aslanovich07 <shavkatxamrayev955@gmail.com>
2022-10-10 13:50:18 +0500

    modul2

--- lib/main.dart
+++ lib/main.dart
@@ -44,42 +44,82 @@ class MyHomePage extends StatelessWidget {
                   ))
             ]),
         drawer: Drawer(),
-        body: Container(
-          width: double.infinity,
-          color: Colors.blue,
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Container(
-                width: 300,
-                height: 200,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(
-                            'https://photos5.appleinsider.com/gallery/30575-50260-iphonexr-red-blackbackground-xl.jpg'),
-                        fit: BoxFit.cover)),
-              ),
-              Container(
-                width: 300,
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            color: Colors.blue,
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Container(
+                  width: 300,
                   height: 200,
-                  child: GridView.count(
-                crossAxisCount: 2,
-                children: [
-                  Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.blue,
-                  ),
-
-
-                ],
-              ))
-            ],
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://photos5.appleinsider.com/gallery/30575-50260-iphonexr-red-blackbackground-xl.jpg'),
+                          fit: BoxFit.cover)),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 140,
+                      height: 150,
+                      color: Colors.black,
+                      margin: EdgeInsets.only(right: 16, left: 14, top: 16),
+                    ),
+                    Container(
+                      width: 140,
+                      height: 150,
+                      color: Colors.black,
+                      decoration: BoxDecoration(image: DecorationImage(image: NetworkImage('url'))),
+                      margin: EdgeInsets.only(
+                        right: 16,
+                        top: 16,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 140,
+                      height: 150,
+                      color: Colors.black,
+                      margin: EdgeInsets.only(right: 16, left: 14, top: 16),
+                    ),
+                    Container(
+                      width: 140,
+                      height: 150,
+                      color: Colors.black,
+                      margin: EdgeInsets.only(
+                        right: 16,
+                        top: 16,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 140,
+                      height: 150,
+                      color: Colors.black,
+                      margin: EdgeInsets.only(right: 16, left: 14, top: 16),
+                    ),
+                    Container(
+                      width: 140,
+                      height: 150,
+                      color: Colors.black,
+                      margin: EdgeInsets.only(
+                        right: 16,
+                        top: 16,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ));
   }
